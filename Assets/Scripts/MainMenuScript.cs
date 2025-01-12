354b37d83d5ecc8a75fd93e1c6dba931af66eff1
jujusrt <judithsort@gmail.com>
2025-01-12 17:37:14 +0100

    Primera part del joc feta
    falta tota la UI i acabar algunes coses de art, fer la plataforma flotante, el objeto que pots recollir. desbloqueable i respawn

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject Gameplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickOnPlay()
+    {
+        Debug.Log("ButtonPressed");
+     //   Gameplay.SetActive(true);
+     //   MainMenu.SetActive(false);
+    }
+}
+
